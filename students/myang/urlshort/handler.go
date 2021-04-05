0f66dda4e94d6346cf65ede29512f578ea58b941
Ming Yang <mgy209@nyu.edu>
2021-04-05 10:55:43 -0400

    Ming's solution

---
+++ students/myang/urlshort/handler.go
@@ -0,0 +1,73 @@
+package urlshort
+
+import (
+	yaml "gopkg.in/yaml.v2"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(writer http.ResponseWriter, reader *http.Request) {
+		path, ok := pathsToUrls[reader.URL.Path]
+		if ok {
+			http.Redirect(writer,reader,path, http.StatusPermanentRedirect)
+		} else {
+			print("Using fallback handler...")
+			fallback.ServeHTTP(writer, reader)
+		}
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yaml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(data []byte) (t []YAMLMap, err error) {
+	err = yaml.Unmarshal(data, &t)
+
+	return t, err
+}
+
+func buildMap(parsedYAML []YAMLMap) map[string]string {
+	m := make(map[string]string)
+	for _, entry := range parsedYAML {
+		m[entry.path] = entry.url
+	}
+
+	return m
+}
+
+type YAMLMap struct {
+	path string `yaml:"path"`
+	url string `yaml:"url"`
+}
+
+func JSONHandler()  {
+	
+}
